de3ed6ef308dd4e1fa16ed90b0e55e2659a61d81
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-09-12 12:18:23 +0100

    update

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 const myObject = {
-  type: 'object',
-  value: 12,
-  incr: function incr() {
-    this.value++;
-  }
-};
-
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+    type: 'object',
+    value: 12,
+    incr: function () {
+      this.value++;
+    }
+  };
+  
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  
