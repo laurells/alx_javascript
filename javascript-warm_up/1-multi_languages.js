26bfdf160633ab2daec5181a409de9b736376543
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-09-12 11:45:58 +0100

    javascript warm up

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+const language1 = "C is fun";
+const language2 = "Python is cool";
+const language3 = "JavaScript is amazing";
+
+console.log(language1);
+console.log(language2);
+console.log(language3);
