3de9b931dfa00975a1ab7563bb0218784f84053c
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-10-02 18:34:16 +0100

    changes

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,20 +1,21 @@
-const fs = require('fs');
-const request = require('request');
+const fs = require("fs");
+const request = require("request");
 
 const url = process.argv[2];
-const filePath = process.argv[3];
+const file_path = process.argv[3];
 
-request(url, (error, response, body) => {
+request(url, function (error, response, body)
+{
+  
   if (error) {
     console.error(error);
     return;
   }
 
-  fs.writeFile(filePath, body, 'utf8', (writeError) => {
-    if (writeError) {
-      console.error(writeError);
+  fs.writeFile(file_path, body, "utf8", function(error) {
+    if (error) {
+      console.error(error);
       return;
     }
-    console.log(`Data from ${url} has been written to ${filePath}`);
   });
 });
